657c4bb719df6bf306ab9fbb85c7d3817cb96bb3
Kh Barsha Singha <khbarshasingha@gmail.com>
2021-04-01 04:21:19 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,20 +7,29 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "./Screens/Home";
 import { Notes } from "./Screens/Notes";
 import { CreateNotes } from "./Screens/CreateNotes";
+import { Row, Col, Container } from "react-bootstrap";
 function App() {
   const [gnotes, setGnotes] = useState([]);
   return (
     <Router>
       <NavBar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/notes">
-          <Notes gnotes={gnotes} setGnotes={setGnotes} />
-        </Route>
-        <Route path="/create">
-          <CreateNotes setGnotes={setGnotes} />
-        </Route>
-      </Switch>
+      <Container>
+        {" "}
+        <Row>
+          <Col sm="12" md="6" lg="6" xl="6">
+            <Home />
+
+            <CreateNotes setGnotes={setGnotes} />
+          </Col>
+          <Col sm="12" md="6" l="6" xl="6">
+            <Switch>
+              <Route path="/notes">
+                <Notes gnotes={gnotes} setGnotes={setGnotes} />
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
     </Router>
   );
 }
--- src/Screens/Notes.js
+++ src/Screens/Notes.js
@@ -1,28 +1,45 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, Col } from "react-bootstrap";
 export const Notes = ({ gnotes, setGnotes }) => {
   function deleteNote(id) {
     setGnotes(gnotes.filter(n => n.id !== id));
   }
   return (
     <div>
-      {gnotes.map(note => {
-        return (
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title>{note.title}</Card.Title>
+      <Row style={{ marginTop: "20px" }}>
+        {" "}
+        <form className="form-inline my-2 my-lg-0 ">
+          <input
+            className="form-control mr-sm-2"
+            type="text"
+            placeholder="Search"
+          />
+          <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </Row>{" "}
+      <Row style={{ marginTop: "20px" }}>
+        {" "}
+        {gnotes.map(note => {
+          return (
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>{note.title}</Card.Title>
 
-              <Card.Text>{note.text}</Card.Text>
-              <i
-                className="fas fa-trash float-right"
-                onClick={() => {
-                  deleteNote(note.id);
-                }}
-              />
-            </Card.Body>
-          </Card>
-        );
-      })}
+                <Card.Text>{note.text}</Card.Text>
+
+                <i
+                  className="fas fa-trash float-right"
+                  onClick={() => {
+                    deleteNote(note.id);
+                  }}
+                />
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </Row>
     </div>
   );
 };
